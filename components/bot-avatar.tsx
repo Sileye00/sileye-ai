4db3891fe54ffb2afd8f0ff9f5cf3f2a817231e3
@Sileye <@dev.Sileyesarr@gmail.com>
2026-03-17 08:01:12 -0500

    fix: bot avatar size and visibility

--- components/bot-avatar.tsx
+++ components/bot-avatar.tsx
@@ -2,8 +2,8 @@ import { Avatar, AvatarImage } from "@/components/ui/avatar"
 
 export const BotAvatar = () => {
   return (
-    <Avatar className="h-10 w-20">
-      <AvatarImage className="p-1" src="/logo.png" />
+    <Avatar className="h-8 w-8 bg-violet-600 p-1">
+      <AvatarImage className="object-contain" src="/logo.png" />
     </Avatar>
   );
 };
